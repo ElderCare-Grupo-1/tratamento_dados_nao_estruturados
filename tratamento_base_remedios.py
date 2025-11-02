17420f63bf07902c1bb77be3f35fe8e6b9ee93f6
Gustavo Luiz <125585314+gustavodessunte@users.noreply.github.com>
2025-11-02 01:20:05 -0300

    Update database connection string for engine

--- tratamento_base_remedios.py
+++ tratamento_base_remedios.py
@@ -99,7 +99,7 @@ if __name__ == '__main__':
     base_remedios_tratada = base_remedios_tratada.to_frame(name='NM_MEDICAMENTO')
     
     try:
-        engine = create_engine("mysql+pymysql://Aluno:Urubu100%40@3.220.74.53:3306/EC_DATA")
+        engine = create_engine("mysql+pymysql://Aluno:Urubu100%40@98.94.4.105:3306/EC_DATA")
         
         with engine.connect() as conn:
             try:
