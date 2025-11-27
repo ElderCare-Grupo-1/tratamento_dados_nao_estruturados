a9ce8c6186608aa3502d97b3af76c798335d78c6
Gustavo Luiz <125585314+gustavodessunte@users.noreply.github.com>
2025-11-27 19:36:06 -0300

    Update database connection IP address

--- tratamento_dados_nestruturados.py
+++ tratamento_dados_nestruturados.py
@@ -274,7 +274,7 @@ def ocr_e_casar(img,
     reader = easyocr.Reader(["pt", "en"], gpu=False)
 
     # carregar base e linhas OCR
-    engine = create_engine("mysql+pymysql://Aluno:Urubu100%40@52.71.62.175:3306/EC_DATA")
+    engine = create_engine("mysql+pymysql://Aluno:Urubu100%40@100.30.117.80:3306/EC_DATA")
     base_norm_map, base_norm_map_num, base_norm_map_nodose, base_norm_list, base_norm_list_num, base_norm_list_nodose, base_word_set = carregar_base_mysql(engine)
     lines = extrair_linhas_ocr(reader, img, preprocess)
     candidates = construir_candidatos_das_linhas(lines, ngram_max=ngram_max)
@@ -345,7 +345,7 @@ if __name__ == '__main__':
     print("Remédios validados:")
     high_conf = [d for d in results_sorted if d.get('combined', 0) > 90]
     print("Remédios com combined > 90:")
-    engine = create_engine("mysql+pymysql://Aluno:Urubu100%40@52.71.62.175:3306/EC_DATA")
+    engine = create_engine("mysql+pymysql://Aluno:Urubu100%40@100.30.117.80:3306/EC_DATA")
 
     if high_conf:
         with engine.begin() as connection:  # transação automática
